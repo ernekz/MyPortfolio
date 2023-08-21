f16cb794f2b993d094526d2767ee91f07628fe88
Ernekz <specialbmx@gmail.com>
2023-08-21 12:40:22 +0200

    Updated About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
             <p className='text-xl mt-20'>
-                I am creative and innovative person willing to get out of my comfort zone to create different types of applications.
-                Mostly I am interested in the backend, but can be working on frontend too, the perfect option would be Full-Stack developer.
+                I am a creative and innovative person who is willing to get out of my comfort zone to create different types of applications.
+                Mostly I am interested in the backend, but can be working on frontend too. The perfect option would be Full-Stack developer.
                 I have experience with many technologies starting with C++ back in the high school days. At the University I gained knowledge 
                 of different technologies that can be found in the Technologies section below.
                 Currently, I am working on my own projects: Travel Application made with react-native, TailwindCSS, and the Backend made with
@@ -23,7 +23,7 @@ const About = () => {
 
             <p className='text-xl'>
                 I am outgoing person which loves to play football, do some running from time to time. On my own time, I like playing computer games,
-                work on my own projects to gain more experience in the field where I want to pursue my career in. I am a person who likes to help people
+                working on my own projects to gain more experience in the field where I want to pursue my career in. I am a person who likes to help people
                 where I can. 
 
             </p>
