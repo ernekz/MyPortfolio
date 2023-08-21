b67863587a439141f28eb44aae8adfb828716f27
Ernekz <specialbmx@gmail.com>
2023-08-21 12:26:03 +0200

    updated my about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,20 +10,21 @@ const About = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
             <p className='text-xl mt-20'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Temporibus eaque voluptas voluptates vitae aperiam ducimus distinctio aliquam eius? 
-                Accusamus, eius qui. Ad odio commodi laboriosam. Praesentium ex, quam harum dolorum aspernatur modi? 
-                Tenetur pariatur asperiores rerum beatae voluptas suscipit eveniet molestias, quam voluptatibus, tempore ratione? 
-                Alias commodi omnis quam. Neque?
-
+                I am creative and innovative person willing to get out of my comfort zone to create different types of applications.
+                Mostly I am interested in the backend, but can be working on frontend too, the perfect option would be Full-Stack developer.
+                I have experience with many technologies starting with C++ back in the high school days. At the University I gained knowledge 
+                of different technologies that can be found in the Technologies section below.
+                Currently, I am working on my own projects: Travel Application made with react-native, TailwindCSS, and the Backend made with
+                Node.js, Express, MySQL.
+                Landlord web application will be made with Next.js
             </p>
 
             <br />
 
             <p className='text-xl'>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Vero dolores eos modi illum accusamus. Repudiandae asperiores quisquam recusandae? 
-                Error eos mollitia perferendis nemo enim laudantium magni soluta iusto qui illum?
+                I am outgoing person which loves to play football, do some running from time to time. On my own time, I like playing computer games,
+                work on my own projects to gain more experience in the field where I want to pursue my career in. I am a person who likes to help people
+                where I can. 
 
             </p>
         </div>
